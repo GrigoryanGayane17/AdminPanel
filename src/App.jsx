4f7482c895c2ef0bef1ce1f09bbc50db3a45d14a
gayanegrigoryan2008 <gayane.grigoryan2024@gmail.com>
2025-08-02 23:26:32 +0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <div>
+            <div className=" bg-black ">
+                Hello, Tailwind with React + Vite!
+            </div>
+        </div>
+    );
+}
+
+export default App;
