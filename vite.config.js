cccd65fd2cfd96691429ee19f4b947538d07f35f
gayanegrigoryan2008 <gayane.grigoryan2024@gmail.com>
2025-08-12 17:39:51 +0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/AdminPanel/',
+  build: {
+    cssCodeSplit: true, // Ensure CSS is properly bundled
+    manifest: true,     // Helps with asset tracking
+  }
 });
